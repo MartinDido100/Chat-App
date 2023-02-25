3102d9be6694a1aea47af457656d24762cdfba1a
Martin Didolich <marindidolich2012@gmail.com>
2023-02-25 00:45:36 -0300

    To Render

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   baseUrl: '/api',
   googleKey: '933510400902-j785qkovvdfi90pidcisfjkf1pbt845i.apps.googleusercontent.com',
-  socketUrl: 'https://friendschat1.azurewebsites.net',
+  socketUrl: 'https://mychat-tkdg.onrender.com',
 };
